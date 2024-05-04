2110da8a62e01f5d3e50b12f1f1103581228140a
KENETGOD <cazareslachinoluisfernando@gmail.com>
2024-05-03 23:08:59 -0600

    Intento de funcionamiento de login

--- php/conexionbd.php
+++ php/conexionbd.php
@@ -12,21 +12,5 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Error en la conexión: " . $conn->connect_error);
 }
-// Si la conexión fue exitosa, puedes realizar operaciones en la base de datos
 
-// Ejemplo de consulta
-$sql = "SELECT * FROM Usuario";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Mostrar los datos de la consulta
-    while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id"] . " - Nombre: " . $row["nombre"] . "<br>";
-    }
-} else {
-    echo "No se encontraron resultados.";
-}
-
-// Cerrar la conexión
-$conn->close();
 ?>
