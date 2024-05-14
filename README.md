736bbd134e4fb9189664207ecf5f00a9db23e6ed
Obed GonHer <l21121524@morelia.tecnm.mx>
2024-05-14 11:38:25 -0600

    adios cambios de main

--- README.md
+++
@@ -1,5 +0,0 @@
-# Proyecto para Programacion Web
-
-## Desdripcion
-
-Este es un simple proyecto de la aplicacion de los conocimientos de clase aplicados a una agina web.
--- php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destruir todas las sesiones
-header("Location: ../index.html"); // Redirigir a la página de inicio de sesión
-exit();
-?>
