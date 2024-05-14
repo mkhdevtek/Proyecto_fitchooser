976e5011dd69e620d08aee2d42f75f2f53e52d05
KENETGOD <cazareslachinoluisfernando@gmail.com>
2024-05-13 23:01:36 -0600

    ABUEBO YA JALO EL REGISTRO DE PRENDA

--- php/upload.php
+++ php/upload.php
@@ -7,5 +7,28 @@ if (!isset($_SESSION['usuario'])) {
     header("Location: ./index.html");
     exit();
 }
+// Recibe los datos del formulario
+$nombre = $_POST['nombre'];
+$descripcion = $_POST['descripcion'];
+$temporada = $_POST['temporada'];
+$categoria = $_POST['categoria'];
+$color = $_POST['color'];
+$imagen = $_FILES['file-upload']['tmp_name'];
+$imagen_contenido = addslashes(file_get_contents($imagen));
 
+// Prepara la consulta SQL
+$sql = "INSERT INTO ropa (nombre, descripcion, color, fotoropa, id_categoria) VALUES (?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql); // Utiliza la conexión establecida en conexion.php
+$stmt->bind_param("sssss", $nombre, $descripcion, $color, $imagen_contenido, $categoria);
+
+// Ejecuta la consulta
+if ($stmt->execute()) {
+    echo "Prenda registrada exitosamente";
+} else {
+    echo "Error al registrar la prenda: " . $stmt->error;
+}
+
+// Cierra la conexión
+$stmt->close();
+$conn->close();
 ?>
